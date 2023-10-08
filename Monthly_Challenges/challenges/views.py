e93d969ff4624f5a3b8e37097fc1e3675dbfce84
WillMcdaniel <125214391+WillMcdaniel@users.noreply.github.com>
2023-10-08 13:17:35 -0700

    Added more dynamic View Logic

--- Monthly_Challenges/challenges/views.py
+++ Monthly_Challenges/challenges/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("This is January")
+#def january(request):
+#    return HttpResponse("This is January")
 
 
-def february(request):
-    return HttpResponse("This is February")
+#def february(request):
+#    return HttpResponse("This is February")
+
+
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(request, month):
+    if month == "january":
+        challenge_text: str = "This is the January challenge"
+    else:
+        return HttpResponseNotFound("This is not the month you are looking for")
+    return HttpResponse(challenge_text)
